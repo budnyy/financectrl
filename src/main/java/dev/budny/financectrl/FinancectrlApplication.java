f9ea46e7ac252592011d3c79e20f2c278650a2cf
Arthur Budny <budnyarthur@gmail.com>
2025-10-09 13:43:41 -0300

    init commit

---
+++ src/main/java/dev/budny/financectrl/FinancectrlApplication.java
@@ -0,0 +1,13 @@
+package dev.budny.financectrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancectrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancectrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/budny/financectrl/FinancectrlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.budny.financectrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancectrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
